5550ae1a24acd36e8eea74fe4952bcd48f5eea57
Logan Gorence <logan@gorence.io>
2019-12-24 15:04:16 -0800

    Add input handlers, commit prior to multi-file split.

--- include/server.h
+++ include/server.h
@@ -9,6 +9,9 @@ struct oak_server {
     struct wl_listener new_output;
     struct wl_list outputs;
 
+    struct wl_listener new_input;
+    struct wl_list inputs;
+
     struct wlr_backend *backend;
     struct wlr_compositor *compositor;
 };
@@ -20,6 +23,15 @@ struct oak_output {
 
     struct wl_listener destroy;
     struct wl_listener frame;
+
+    struct wl_list link;
+};
+
+struct oak_input_device {
+    struct wlr_input_device *wlr_input_device;
+    struct oak_server *server;
+
+    struct wl_listener destroy;
 
     struct wl_list link;
 };
--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_gtk_primary_selection.h>
 #include <wlr/types/wlr_idle.h>
+#include <wlr/types/wlr_input_device.h>
 #include <wlr/types/wlr_matrix.h>
 #include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_xdg_shell_v6.h>
@@ -33,7 +34,7 @@ static void output_frame_notify(struct wl_listener *listener, void *data) {
     wlr_output_attach_render(wlr_output, NULL);
     wlr_renderer_begin(renderer, wlr_output->width, wlr_output->height);
 
-    float color[4] = {1.0f, 0.0f, 0.0f, 1.0f};
+    float color[4] = {0.0f, 0.0f, 0.0f, 1.0f};
     wlr_renderer_clear(renderer, color);
 
     struct wl_resource *_surface;
@@ -81,6 +82,41 @@ static void new_output_notify(struct wl_listener *listener, void *data) {
     wlr_output_create_global(wlr_output);
 }
 
+static void input_destroy_notify(struct wl_listener *listener, void *data) {
+    struct oak_input_device *input = wl_container_of(listener, input, destroy);
+    wl_list_remove(&input->link);
+    wl_list_remove(&input->destroy.link);
+    free(input);
+}
+
+static void new_input_notify(struct wl_listener *listener, void *data) {
+    struct oak_server *server = wl_container_of(listener, server, new_input);
+    struct wlr_input_device *wlr_input_device = data;
+    printf("New input device: %s\n", wlr_input_device->name);
+
+    switch (wlr_input_device->type) {
+        case WLR_INPUT_DEVICE_KEYBOARD:
+            printf("Detected as keyboard.\n");
+            break;
+        case WLR_INPUT_DEVICE_POINTER:
+            printf("Detected as pointer.\n");
+            break;
+        case WLR_INPUT_DEVICE_TOUCH:
+            printf("Detected as touch.\n");
+            break;
+        default:
+            break;
+    }
+
+    struct oak_input_device *input = calloc(1, sizeof(struct oak_input_device));
+    input->server = server;
+    input->wlr_input_device = wlr_input_device;
+
+    wl_list_insert(&server->inputs, &input->link);
+    input->destroy.notify = input_destroy_notify;
+    wl_signal_add(&wlr_input_device->events.destroy, &input->destroy);
+}
+
 int main(void) {
     struct oak_server server;
 
@@ -95,6 +131,10 @@ int main(void) {
     wl_list_init(&server.outputs);
     server.new_output.notify = new_output_notify;
     wl_signal_add(&server.backend->events.new_output, &server.new_output);
+
+    wl_list_init(&server.inputs);
+    server.new_input.notify = new_input_notify;
+    wl_signal_add(&server.backend->events.new_input, &server.new_input);
 
     const char *socket = wl_display_add_socket_auto(server.wl_display);
     assert(socket);
@@ -115,6 +155,8 @@ int main(void) {
     server.compositor = wlr_compositor_create(server.wl_display, wlr_backend_get_renderer(server.backend));
 
     wlr_xdg_shell_v6_create(server.wl_display);
+
+    system("gnome-terminal -- htop &");
 
     wl_display_run(server.wl_display);
     wl_display_destroy(server.wl_display);
